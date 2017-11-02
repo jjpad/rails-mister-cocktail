041a082d862a2b66a828a5a1ae549b8124b2e47b
Jun Joseph Padilla <jun.joseph.padilla@gmail.com>
2017-11-02 21:43:09 +0100

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,11 @@
 Rails.application.routes.draw do
-  # get 'doses/index'
-
-  # get 'doses/new'
-
-  # get 'doses/create'
-
-  # get 'doses/edit'
-
-  # get 'doses/update'
-
-  # get 'doses/destroy'
-
-  # get 'cocktails/index'
-
-  # get 'cocktails/show'
-
-  # get 'cocktails/new'
-
-  # get 'cocktails/create'
-
-  # get 'cocktails/edit'
-
-  # get 'cocktails/update'
-
-  # get 'cocktails/destroy'
 
   resources :cocktails do
     resources :doses , only: [:new, :create]
   end
   resources :doses, only: [:destroy]
+
+  # root 'cocktails#index'
 # we want to not destory the cocktail id so delete for dose is separate
 
 end
