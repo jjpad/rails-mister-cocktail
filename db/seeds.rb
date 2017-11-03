cbbf4113836ae07a887ef431c8732b73b307d9f3
Jun Joseph Padilla <jun.joseph.padilla@gmail.com>
2017-11-03 10:39:44 +0100

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,24 @@
-# require 'json'
-# require 'open-uri'
-# require 'pry'
+require 'json'
+require 'open-uri'
+require 'pry'
 
-# url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-# data_serialized = open(url).read
-# data = JSON.parse(data_serialized)
+Ingredient.destroy_all
 
-# data["drinks"].each do |name|
-#   name = element["strIngredient1"]
-#   ingr = Ingredient.create(name: name)
-#   puts "#{ingr.name}"
-# end
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data_serialized = open(url).read
+data = JSON.parse(data_serialized)
+
+data["drinks"].each do |name|
+  name = element["strIngredient1"]
+  ingr = Ingredient.create(name: name)
+  puts "#{ingr.name}"
+end
 # binding.pry
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+# Ingredient.destroy_all
+# Cocktail.destroy_all
+
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
