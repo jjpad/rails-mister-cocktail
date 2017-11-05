8202f2e0f59f2d72d83bd1a5981a678f8f1f8c5f
Jun Joseph Padilla <jun.joseph.padilla@gmail.com>
2017-11-05 22:31:45 +0100

    functioning cocktail app on localhost - haven't checked heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,13 @@ url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 data_serialized = open(url).read
 data = JSON.parse(data_serialized)
 
-data["drinks"].each do |name|
+data["drinks"].each do |element|
   name = element["strIngredient1"]
   ingr = Ingredient.create(name: name)
-  puts "#{ingr.name}"
+  puts "#{ingr.name} created!"
 end
-# binding.pry
+puts "Finished seeding"
+
 
 # Ingredient.destroy_all
 # Cocktail.destroy_all
@@ -22,3 +23,5 @@ end
 # Ingredient.create(name: "lemon")
 # Ingredient.create(name: "ice")
 # Ingredient.create(name: "mint leaves")
+
+
